7d4e28470b15a5123c8e25db9fb81fdcf7ad1c0d
Hanna Lindgren <han.lind@gmail.com>
2026-04-23 12:36:06 +0200

    investigating for memory leaks

--- matcha/cli.py
+++ matcha/cli.py
@@ -43,7 +43,7 @@ def plot_spectrogram_to_numpy(spectrogram, filename):
     plt.title("Synthesised Mel-Spectrogram")
     fig.canvas.draw()
     plt.savefig(filename)
-
+    plt.close(fig)
 
 def process_text(i: int, text: str, device: torch.device):
     print(f"[{i}] - Input text: {text}")
@@ -125,7 +125,7 @@ def to_waveform(mel, vocoder, denoiser=None, denoiser_strength=0.00025):
 def save_to_folder(filename: str, output: dict, folder: str):
     folder = Path(folder)
     folder.mkdir(exist_ok=True, parents=True)
-    plot_spectrogram_to_numpy(np.array(output["mel"].squeeze().float().cpu()), folder / f"{filename}.png")
+    #plot_spectrogram_to_numpy(np.array(output["mel"].squeeze().float().cpu()), folder / f"{filename}.png")
     np.save(folder / f"{filename}", output["mel"].cpu().numpy())
     sf.write(folder / f"{filename}.wav", output["waveform"], 22050, "PCM_24")
     return folder.resolve() / f"{filename}.wav"
